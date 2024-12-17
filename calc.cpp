6d11aa2d2fdd7ca2184a5032d0b2d610d4dd3b98
brazy <artrem99k5@gmail.com>
2024-12-18 01:10:18 +0300

    fun work

---
+++ calc.cpp
@@ -0,0 +1,5 @@
+#include "calc.h"
+
+Calculator::Calculator(QWidget *parent) : QWidget(parent){
+
+}
---
+++ numpad.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <QWidget>
+
+class Numpad : public QWidget {
+    Q_OBJECT
+
+public:
+    Numpad(QWidget *parent = nullptr);
+
+private:
+
+};
